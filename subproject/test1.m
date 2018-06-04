8baa16371df6af411c25092799c35147f102d051
niuxinghua <niuxinghua@haier.com>
2018-06-04 13:25:54 +0800

    no message
    Change-Id: Ia93706968f19685b235101c8cb2d83a7851db9ef

---
+++ subproject/test1.m
@@ -0,0 +1,13 @@
+//
+//  test1.m
+//  模块化子工程
+//
+//  Created by niuxinghua on 2018/6/4.
+//  Copyright © 2018年 com.haier. All rights reserved.
+//
+
+#import "test1.h"
+
+@implementation test1
+
+@end
