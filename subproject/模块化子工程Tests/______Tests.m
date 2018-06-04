3a3f4ca3c35432016c27cbe556fcc4322791a3e0
niuxinghua <niuxinghua@haier.com>
2018-06-04 10:43:21 +0800

    init
    Change-Id: I07eabee6e0d3e97eeba1b362670fb100bd2272b5

---
+++ subproject/模块化子工程Tests/______Tests.m
@@ -0,0 +1,39 @@
+//
+//  ______Tests.m
+//  模块化子工程Tests
+//
+//  Created by niuxinghua on 2018/6/4.
+//  Copyright © 2018年 com.haier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ______Tests : XCTestCase
+
+@end
+
+@implementation ______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
